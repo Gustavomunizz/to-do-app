3c3b6a61e8828efbafa9422ba7df420bba12c5c8
Gustavo Muniz <gustamunizmartins@gmail.com>
2024-01-12 19:35:39 -0300

    atualizando configs

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,10 @@ module.exports = {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
+    static: {
+      directory: path.resolve(__dirname, 'public')
+    },
+   
     hot: true,
   },
   plugins: [
